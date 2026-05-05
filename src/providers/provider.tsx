f2b57f1b3631f4ba7e81875e5a84e60e3312c368
Ifal Fahri A <ifalfahri16@gmail.com>
2026-05-05 19:13:39 +0800

    feat: integrate GSAP for enhanced navbar animations

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { LenisRef, ReactLenis } from 'lenis/react'
 import { useEffect, useRef } from 'react'
 
@@ -12,13 +13,22 @@ export const Provider = ({ children }: ProviderProps) => {
     const lenisRef = useRef<LenisRef | null>(null)
 
     useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+
+        const onLenisScroll = () => ScrollTrigger.update()
+
         function update(time: number) {
             lenisRef.current?.lenis?.raf(time * 1000)
         }
 
+        lenisRef.current?.lenis?.on('scroll', onLenisScroll)
         gsap.ticker.add(update)
+        gsap.ticker.lagSmoothing(0)
 
-        return () => gsap.ticker.remove(update)
+        return () => {
+            lenisRef.current?.lenis?.off('scroll', onLenisScroll)
+            gsap.ticker.remove(update)
+        }
     }, [])
 
     return (
